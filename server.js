87f67247ced56d51040ad13364efbdeafa50e73c
saniP <sanipatel0401@gmail.com>
2023-12-21 17:11:48 +0530

    Updated

--- server.js
+++ server.js
@@ -25,25 +25,16 @@ pool.query("SELECT NOW()", (err, result) => {
   }
 });
 
-// Route that queries the database for the first 100 rows
-app.get("/getData", async (req, res) => {
-  console.log("Request received");
-
-  const { state, district, tehsil, village, lgd_code, khasra_no } = req.query;
+// Query to get ogc_fid from global_india_khasra
+app.get("/adjData", async (req, res) => {
+  const { saltKey, state, district, tehsil, village, lgd_code, khasra_no } =
+    req.query;
 
   try {
-    const queryResult = await pool.query(
-      `
-      SELECT
-        ST_AsGeoJSON(geom) AS geometry,
-        state,
-        district,
-        tehsil,
-        village,
-        khasra_no,
-        area_ac,
-        shape_leng,
-        shape_area
+    if (saltKey == "c4f1e08a2b5d1e3f8d9a0b6c3e2d5a1") {
+      const queryResult = await pool.query(
+        `
+      SELECT ogc_fid
       FROM global_india_khasra
       WHERE
         state = $1 AND
@@ -52,40 +43,105 @@ app.get("/getData", async (req, res) => {
         village = $4 AND
         lgd_code = $5 AND
         khasra_no = $6
-      LIMIT 100
+      LIMIT 1
     `,
-      [state, district, tehsil, village, lgd_code, khasra_no]
-    );
+        [state, district, tehsil, village, lgd_code, khasra_no]
+      );
 
-    // Transform the result into GeoJSON format
-    const features = queryResult.rows.map((row) => {
-      return {
-        type: "Feature",
-        geometry: JSON.parse(row.geometry),
-        properties: {
-          fid: row.fid,
-          objectid: row.objectid,
-          State: row.state,
-          District: row.district,
-          Tehsil: row.tehsil,
-          Village: row.village,
-          Khasra_No: row.khasra_no,
-          Area_ac: row.area_ac,
-          Shape_Leng: row.shape_leng,
-          Shape_Area: row.shape_area,
-          path: row.path,
-          layer: row.layer,
-          new_area_a: row.new_area_a,
-        },
+      if (queryResult.rows.length === 0) {
+        res.status(404).send("Record not found");
+        return;
+      }
+
+      const ogc_fid = queryResult.rows[0].ogc_fid;
+
+      // Now use ogc_fid in the second query
+      const customQueryResult = await pool.query(
+        `
+      SELECT a.khasra_no, ST_AsText(ST_GeomFromWKB(ST_Centroid(a.geom)))
+      FROM test1 AS a
+      JOIN test1 AS b ON ST_Intersects(ST_Buffer(a.geom, 0.002), b.geom)
+      WHERE b.ogc_fid =$1 AND a.ogc_fid != $1;
+    `,
+        [ogc_fid]
+      );
+
+      // Process the result as needed
+      res.json(customQueryResult.rows);
+    } else {
+      res.json({ message: "Unauthorised Access" });
+    }
+  } catch (error) {
+    console.error("Error executing queries", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/getData", async (req, res) => {
+  console.log("Request received");
+
+  const { saltKey, state, district, tehsil, village, lgd_code, khasra_no } =
+    req.query;
+
+  try {
+    if (saltKey === "EgNIgHpqbW3ja1EgWrsPC1c4FQgJukYs9jhlswdC") {
+      const queryResult = await pool.query(
+        `
+        SELECT
+          ST_AsGeoJSON(geom) AS geometry,
+          state,
+          district,
+          tehsil,
+          village,
+          khasra_no,
+          area_ac,
+          shape_leng,
+          shape_area
+        FROM global_india_khasra
+        WHERE
+          state = $1 AND
+          district = $2 AND
+          tehsil = $3 AND
+          village = $4 AND
+          lgd_code = $5 AND
+          khasra_no = $6
+        LIMIT 100
+      `,
+        [state, district, tehsil, village, lgd_code, khasra_no]
+      );
+
+      // Transform the result into GeoJSON format
+      const features = queryResult.rows.map((row) => {
+        return {
+          type: "Feature",
+          geometry: JSON.parse(row.geometry),
+          properties: {
+            fid: row.fid,
+            objectid: row.objectid,
+            State: row.state,
+            District: row.district,
+            Tehsil: row.tehsil,
+            Village: row.village,
+            Khasra_No: row.khasra_no,
+            Area_ac: row.area_ac,
+            Shape_Leng: row.shape_leng,
+            Shape_Area: row.shape_area,
+            path: row.path,
+            layer: row.layer,
+            new_area_a: row.new_area_a,
+          },
+        };
+      });
+
+      const geoJsonResponse = {
+        type: "FeatureCollection",
+        features: features,
       };
-    });
 
-    const geoJsonResponse = {
-      type: "FeatureCollection",
-      features: features,
-    };
-
-    res.json(geoJsonResponse);
+      res.json(geoJsonResponse);
+    } else {
+      res.status(401).json({ message: "Unauthorized Access" });
+    }
   } catch (error) {
     console.error("Error executing query", error);
     res.status(500).send("Internal Server Error");
@@ -93,5 +149,5 @@ app.get("/getData", async (req, res) => {
 });
 
 app.listen(6000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 6000");
 });
